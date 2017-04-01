065f307f7d0ddad9394d1fc8ace0c3c3c5c75a29
raphaklaus <raphaklaus@gmail.com>
2017-04-01 11:01:14 -0300

    feat: add raw reader to cpu

--- RawReader.js
+++ RawReader.js
@@ -1,14 +1,12 @@
 const fs = require('bluebird').promisifyAll(require('fs'));
 
-class RawReader {
+module.exports = class RawReader {
   static async load() {
     try {
       let file = await fs.readFileAsync('./test/roms/PokeRed.gb');
-      console.log(file);
+      return file
     } catch (error) {
       console.log('Something went wrong!', error);
     }
   }
 }
-
-RawReader.load();
--- cpu.js
+++ cpu.js
@@ -1,27 +1,27 @@
+const rom = require('./RawReader.js');
+
 class CPU {
   constructor() {
     this.registers = {
-      a: 0x00,
-      b: '',
-      c: '',
-      d: '',
-      e: '',
-      f: 0xb0,
-      h: '',
-      l: '',
-      af: 0x01,
+      af: 0x01b0,
       bc: 0x13,
       de: 0xd8,
-      hl: 0x14,
+      hl: 0x14D,
       sp: 0xfffe,
       pc: 0x100
     };
 
     this.flags = {
-      z: '',
-      n: '',
-      h: '',
-      c: ''
+      zero: '',
+      subtract: '',
+      halfCarry: '',
+      carry: ''
     };
+
+    this.memory = new Uint8Array(0x10000);
+
+    rom.load();
   }
 }
+
+var gameBoy = new CPU();
