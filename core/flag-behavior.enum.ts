ba434286d787b797ec04d6ccf78571704f4b430a
Raphael Klaus <raphaklaus@gmail.com>
2018-09-13 13:17:57 -0300

    feat(core): add register and flag interfaces

---
+++ core/flag-behavior.enum.ts
@@ -0,0 +1,6 @@
+export enum FlagBehavior {
+  Zero = 'zero',
+  One = 'one',
+  Context = 'context',
+  NotAffected = 'not affected'
+}
---
+++ core/register-semantics.enum.ts
@@ -0,0 +1,4 @@
+export enum RegisterSemantics {
+  Value = 'value',
+  Memory = 'memory'
+}
