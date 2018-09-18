4551ac0224b36059bb143bb28f8ad44d09f53e3b
Raphael Klaus <raphaklaus@gmail.com>
2018-09-18 02:26:26 -0300

    fix(emulator): cpu method run to be awaited

--- src/emulator/index.ts
+++ src/emulator/index.ts
@@ -3,9 +3,9 @@ import { createMemory } from './memory';
 import { run } from './cpu';
 
 (async () => {
-  const rom = new Uint8Array(await promises.readFile(__dirname + '/../../roms/Tetris.gb'))
+  const rom = new Uint8Array(await promises.readFile(__dirname + '/../../roms/bootstrap.gb'))
   const memory = createMemory()
   console.log('rom', rom[0])
   console.log('memory space', memory.length)
-  run(rom)
+  await run(rom)
 })()
